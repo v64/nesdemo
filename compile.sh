c5f95e1b6f9edb695ab8f2d34d61b399642ffddb
Jahn Veach <j@hnvea.ch>
2019-07-14 13:50:06 -0400

    sdl

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# brew install sdl
+
 gcc -O3 -Wall \
-	-lSDL -I/usr/local/Cellar/sdl/1.2.15/include/SDL/ -I./include \
+	-lSDL -I/usr/local/Cellar/sdl/1.2.15_1/include/SDL/ -I./include \
 	nes.c memory.c cpu.c ppu.c \
 	main.c video.c input.c \
 	-o nesdemo
